6c08ede880d092a5e0a4390d970be8a14ab09165
dhanasekar1418 <dhanasekar.v2022eee@sece.ac.in>
2025-09-02 20:58:21 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express(); 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ["http://localhost:5173", "https://onlinemart-theta.vercel.app"], 
   credentials: true,               
 }));
 app.use(express.json());
