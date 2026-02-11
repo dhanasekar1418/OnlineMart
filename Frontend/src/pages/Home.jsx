bc463e8fe48b2416df9d2535cf44f073e6bd7e87
dhanasekar1418 <dhanasekar.v2022eee@sece.ac.in>
2026-02-11 10:21:29 +0530

    final commit

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import BannerCarousel from '../components/BannerCarousel';
 import '../Styles/home.css';
 import { useNavigate } from 'react-router-dom';
+import categoriesImage from '../assets/categories.png';
+import pageimage from '../assets/side.png';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -19,6 +21,13 @@ const Home = () => {
       {/* Main Content */}
       <div className="main-content">
         <BannerCarousel />
+         {/* Categories Section */}
+      <div className="categories-section" onClick={() => navigate('/products')}>
+        <img src={categoriesImage} alt="Categories" />
+      </div>
+      <div className="sidepage" onClick={() => navigate('/products')}>
+        <img src={pageimage} alt="sidepage" />
+      </div>
       </div>
     </div>
   );
